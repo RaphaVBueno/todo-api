809044ac6041d5ac7deee91eb4880e837d0cc185
Raphael V. Bueno <rafavbueno@gmail.com>
2024-10-26 09:49:45 -0300

    atualização de usuario

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,8 +2,9 @@ import express, { Request, Response } from 'express'
 import { PrismaClient, Usuario } from '@prisma/client'
 import { findUserError, isNumber } from 'src/utils'
 import bcrypt from 'bcrypt'
-import { numberValidation } from 'src/validations'
+import { dateValidation, numberValidation } from 'src/validations'
 import { auth } from 'src/middlewares'
+import { aw } from 'vitest/dist/chunks/reporters.C_zwCd4j'
 
 const prisma = new PrismaClient()
 
@@ -44,12 +45,23 @@ export async function getUser(req: Request, res: Response) {
 export async function addUser(req: Request, res: Response) {
   try {
     const { name, email, password, birthDate, username } = req.body
+
+    dateValidation(birthDate)
+
     const existingUser = await prisma.usuario.findUnique({
       where: { email },
     })
-
     if (existingUser) {
       return res.status(400).json({ message: 'e-mail já está em uso.' })
+    }
+
+    const existingUsername = await prisma.usuario.findUnique({
+      where: { username },
+    })
+    if (existingUsername) {
+      return res
+        .status(400)
+        .json({ message: 'nome do usuário já está em uso.' })
     }
 
     const salt = await bcrypt.genSalt(10)
@@ -74,6 +86,7 @@ export async function addUser(req: Request, res: Response) {
 export async function deleteUser(req: Request, res: Response) {
   try {
     const { id } = req.body
+
     isNumber(id)
     await findUserError(id)
 
@@ -91,10 +104,24 @@ export async function deleteUser(req: Request, res: Response) {
 
 export async function updateUser(req: Request, res: Response) {
   try {
-    const { name, password, id, email } = req.body
-    isNumber(id)
-    await findUserError(id)
+    const { name, password } = req.body
+    let { email } = req.body
+    const { id } = req.body.context.user as Usuario
 
+    const userEmail = await prisma.usuario.findUnique({
+      where: { id },
+    })
+
+    if (email === userEmail?.email) {
+      email = undefined
+    } else {
+      const existingEmail = await prisma.usuario.findUnique({
+        where: { email },
+      })
+      if (existingEmail)
+        return res.status(400).json({ message: 'email já está em uso.' })
+    }
+    //fazer a mesma logica com password
     const user = await prisma.usuario.update({
       where: { id },
       data: {
@@ -115,12 +142,12 @@ const router = express.Router()
 
 router.get('/', getUserList)
 
-router.get('/me', auth, getUser) // /me
+router.get('/me', auth, getUser)
 
 router.post('/add', addUser)
 
 router.delete('/', deleteUser)
 
-router.put('/', updateUser)
+router.put('/', auth, updateUser)
 
 export default router
